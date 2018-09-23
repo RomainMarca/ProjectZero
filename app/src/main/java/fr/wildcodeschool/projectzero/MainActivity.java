390bd8e72b7dca85846f41c1e81da5fd0faf9c0e
Romain Marca <rmarca@hotmail.fr>
2018-09-23 16:52:58 +0200

    add opening page

--- app/src/main/java/fr/wildcodeschool/projectzero/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/projectzero/MainActivity.java
@@ -1,7 +1,12 @@
 package fr.wildcodeschool.projectzero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button Enter = findViewById(R.id.button_enter);
+
+
+
+        Enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView description = findViewById(R.id.text_description);
+                description.setVisibility(View.VISIBLE);
+
+                Button RealEnter = findViewById(R.id.button_realenter);
+                RealEnter.setVisibility(View.VISIBLE);
+
+
+
+                RealEnter.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent goToroom1 = new Intent(MainActivity.this , Room1.class);
+                        MainActivity.this.startActivity(goToroom1);
+
+                    }
+                });
+
+            }
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
