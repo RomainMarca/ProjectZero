5cb4662e84aa15465364820540873ece726d617d
Romain Marca <rmarca@hotmail.fr>
2018-09-28 14:04:07 +0200

    test

--- app/src/main/java/fr/wildcodeschool/projectzero/Event.java
+++ app/src/main/java/fr/wildcodeschool/projectzero/Event.java
@@ -1,33 +1,25 @@
 package fr.wildcodeschool.projectzero;
 
+import android.widget.ImageView;
+
 public class Event {
 
-    private boolean Icon;
+
     private boolean Action;
 
-    public Event(boolean Icon, boolean Action){
-        this.Icon = Icon;
-        this.Action = Action;
+
+    public Event(boolean action){
+        this.Action = action;
     }
 
     //getter
-    public boolean isIcon() {
-        return Icon;
-    }
-
     public boolean isAction() {
         return Action;
     }
 
     //setter
-
-    public void setIcon(boolean icon) {
-        Icon = icon;
-    }
-
     public void setAction(boolean action) {
-        Action = action;
+        action = action;
     }
-
 
 }
