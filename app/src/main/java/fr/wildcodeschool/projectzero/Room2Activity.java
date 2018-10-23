8539618ea7fbd9511f7043adb013dd21bb823ea2
Romain Marca <rmarca@hotmail.fr>
2018-10-23 16:20:44 +0200

    advanced room2

--- app/src/main/java/fr/wildcodeschool/projectzero/Room2Activity.java
+++ app/src/main/java/fr/wildcodeschool/projectzero/Room2Activity.java
@@ -1,9 +1,9 @@
 package fr.wildcodeschool.projectzero;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,14 +13,15 @@ import android.widget.TextView;
 public class Room2Activity extends AppCompatActivity {
 
     public int counter;
+    //TODO make the background complete for build in room2.xml
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room2);
 
-
         final ConstraintLayout background = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        
         //Metthode arguments showEvent
         final TextView textEvent = findViewById(R.id.text_event);
         final Button X = findViewById(R.id.button_hollow);
@@ -37,18 +38,27 @@ public class Room2Activity extends AppCompatActivity {
         counter = bundle.getInt("counter", counter);
         Counter2.setText(String.valueOf(counter));
 
+        showEvent(true, textEvent, X);
+        textEvent.setText(R.string.enterRoom2);
+        X.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEvent(false, textEvent, X);
+            }
+        });
+
         imgObsLockerR2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 // Show and close textEvent
-                showEvent(true,textEvent, X);
+                showEvent(true, textEvent, X);
                 textEvent.setText(R.string.ObsLockerR2);
 
                 X.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        showEvent(false,textEvent, X);
+                        showEvent(false, textEvent, X);
                         imgObsLockerR2.setVisibility(View.INVISIBLE);
                         imgActionLocker1R2.setVisibility(View.VISIBLE);
                         imgActionLocker2R2.setVisibility(View.VISIBLE);
@@ -65,30 +75,29 @@ public class Room2Activity extends AppCompatActivity {
             }
         });
 
-    imgGoRoom1.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent goToRoom1 = new Intent(Room2Activity.this, Room1.class);
-            Room2Activity.this.startActivity(goToRoom1);
-        }
-    });
-
+        imgGoRoom1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRoom1 = new Intent(Room2Activity.this, Room1.class);
+                Room2Activity.this.startActivity(goToRoom1);
+            }
+        });
 
 
     }
 
 
-    public void isVisited(boolean visited, ConstraintLayout background){
-        if(visited){
+    public void isVisited(boolean visited, ConstraintLayout background) {
+        if (visited) {
             background.setBackgroundResource(R.drawable.room2);
         }
     }
 
-    public void showEvent(boolean openEvent, TextView textEvent, Button X){
-        if(openEvent){
+    public void showEvent(boolean openEvent, TextView textEvent, Button X) {
+        if (openEvent) {
             textEvent.setVisibility(View.VISIBLE);
             X.setVisibility(View.VISIBLE);
-        }else{
+        } else {
             textEvent.setVisibility(View.INVISIBLE);
             X.setVisibility(View.INVISIBLE);
         }
